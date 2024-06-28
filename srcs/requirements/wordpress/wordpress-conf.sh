03335f504ce4dfa22fcede93bacbfca44ef3bc30
rscres <rseelaen@student.42sp.org.br>
2024-06-28 01:39:27 -0300

    containers running but wordpress can't connect to mariadb

--- srcs/requirements/wordpress/wordpress-conf.sh
+++ srcs/requirements/wordpress/wordpress-conf.sh
@@ -1,24 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 #---------------------------------------------------wp installation---------------------------------------------------#
-# wp-cli installation
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# wp-cli permission
-chmod +x wp-cli.phar
-# wp-cli move to bin
-mv wp-cli.phar /usr/local/bin/wp
 
 # go to wordpress directory
 cd /var/www/wordpress
 # give permission to wordpress directory
 chmod -R 755 /var/www/wordpress/
-# change owner of wordpress directory to www-data
-chown -R www-data:www-data /var/www/wordpress
+# change owner of wordpress directory to nginx
+adduser --no-create-home nginx nginx
+chown -R nginx:nginx /var/www/wordpress
 #---------------------------------------------------ping mariadb---------------------------------------------------#
 # check if mariadb container is up and running
 ping_mariadb_container() {
     nc -zv mariadb 3306 > /dev/null # ping the mariadb container
     return $? # return the exit status of the ping command
 }
+
 start_time=$(date +%s) # get the current time in seconds
 end_time=$((start_time + 20)) # set the end time to 20 seconds after the start time
 while [ $(date +%s) -lt $end_time ]; do # loop until the current time is greater than the end time
@@ -38,7 +34,7 @@ fi
 #---------------------------------------------------wp installation---------------------------------------------------#
 
 # download wordpress core files
-wp core download --allow-root
+# wp core download --allow-root
 # create wp-config.php file with database details
 wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
 # install wordpress with the given title, admin username, password and email
@@ -49,8 +45,8 @@ wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U
 #---------------------------------------------------php config---------------------------------------------------#
 
 # change listen port from unix socket to 9000
-sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's@/run/php82/php-fpm.sock@9000@' /etc/php82/php-fpm.d/www.conf
 # create a directory for php-fpm
 mkdir -p /run/php
 # start php-fpm service in the foreground to keep the container running
-/usr/sbin/php-fpm7.4 -F
+php-fpm82 -F
