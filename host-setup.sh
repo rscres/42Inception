f91270906b41ffda0336e5eb0b3f34fa509304ab
rscres <rseelaen@student.42sp.org.br>
2024-07-14 19:24:05 -0300

    add port 8080:80 to wordpress in docker-compose, fix script to add domain to /etc/hosts, mandatory part done

--- host-setup.sh
+++ host-setup.sh
@@ -3,7 +3,8 @@
 # Check if the entry already exists
 if ! grep -q "rseelaen.42.fr" /etc/hosts; then
   # Append the entry to the hosts file
-  echo "127.0.0.1    rseelaen.42.fr" | sudo -S tee -a /etc/hosts <<< "user42" > /dev/null
+  # echo "127.0.0.1    rseelaen.42.fr" | sudo -S tee -a /etc/hosts <<< "user42" > /dev/null
+  echo user42 | sudo -S sleep 1 && sudo sed -i '/localhost/s/$/ rseelaen.42.fr/' /etc/hosts
   echo "Hosts file updated."
 else
   echo "Entry already exists."
