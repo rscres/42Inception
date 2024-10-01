71e840f30f399fccc91203680cc33e55896deeaf
rscres <rseelaen@student.42sp.org.br>
2024-10-01 20:43:56 -0300

    Nginx: change from /www/wordpress to /www/html/ Mariadb: remove old version Wordpress: change conf script to simplified version, only sets up the website and users and Dockerfile add wp core download command Docker-compose: change /www/wordpress to /www/html, change wordpress port from 8080:80 to 9000:9000

--- srcs/requirements/wordpress/tools/wordpress-conf.sh
+++ srcs/requirements/wordpress/tools/wordpress-conf.sh
@@ -1,27 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
+wp config create --allow-root --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD"  --dbhost=mariadb --dbprefix="wp_"
 
-####### MANDATORY PART ##########
+wp core install --allow-root --path=/var/www/html --title="$WP_TITLE" --url=$DOMAIN_NAME --admin_user=$WP_ADMIN_N --admin_password=$WP_ADMIN_P --admin_email=$WP_ADMIN_E
 
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+wp user create --allow-root --path=/var/www/html "$WP_U_NAME" "$WP_U_EMAIL" --user_pass=$WP_U_PASS  --role=$WP_U_ROLE
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-###################################
-fi
+wp --allow-root --activate theme install astra
 
-exec "$@"
+exec php-fpm7.4 -F
